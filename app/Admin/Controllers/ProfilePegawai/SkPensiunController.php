544066f891ab09429bb552cf6c2c2ef558673972
koharudin <koharudin.mail07@gmail.com>
2023-07-14 07:35:18 +0700

    sk pensiun

--- app/Admin/Controllers/ProfilePegawai/SkPensiunController.php
+++ app/Admin/Controllers/ProfilePegawai/SkPensiunController.php
@@ -3,6 +3,8 @@
 namespace App\Admin\Controllers\ProfilePegawai;
 
 use App\Models\Employee;
+use App\Models\JenisPensiun;
+use App\Models\Pangkat;
 use App\Models\RiwayatPensiun;
 use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Content;
@@ -27,8 +29,16 @@ class SkPensiunController extends  ProfileController
         $form = new Form(new RiwayatPensiun());
         $form->hidden('employee_id', 'ID');
         // Add an input box of type text
-        $form->text('no_sk', 'NO SK');
-        $form->date('tmt_pensiun', 'TMT Pensiun');
+        $form->text('no_bkn', 'NO BKN');
+        $form->date('tgl_bkn', 'TGL BKN');
+        $form->text('no_sk', 'NO SK PENSIUN')->required(true);
+        $form->date('tgl_pensiun', 'TANGGAL PENSIUN')->required(true);
+        $form->date('tmt_pensiun', 'TMT PENSIUN')->required(true);
+        $form->select('pangkat_id', 'PANGKAT (GOL RUANG)')->options(Pangkat::all()->pluck('name','id'));
+        $form->text('masa_kerja_tahun', 'MASA KERJA TAHUN');
+        $form->text('masa_kerja_bulan', 'MASA KERJA BULAN');
+        $form->text('unit_kerja', 'UNIT KERJA');
+
         $form->tools(function ($tools) {
             $tools->disableList();
             $tools->disableView();
