b3fc8513c4939387425b43660138e3c2d57589ef
koharudin <koharudin.mail07@gmail.com>
2023-07-16 20:06:05 +0700

    dashboard profile

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -19,16 +20,21 @@ class HomeController extends Controller
             ->row(function (Row $row) {
 
                 $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
-                });
-
-                $row->column(4, function (Column $column) {
-                    //$column->append(Dashboard::extensions());
-                });
-
-                $row->column(4, function (Column $column) {
-                    //$column->append(Dashboard::dependencies());
+                    $column->append(self::user_profile()); 
                 });
             });
+    }
+    protected static function user_profile()
+    {
+        $user = Auth::user();
+        $roles = $user->roles->pluck('name');
+        $roles = implode(',',$roles->toArray());
+        $envs = [
+            ['name' => 'Username',       'value' => $user->username],
+            ['name' => 'Nama',       'value' => $user->name],
+            ['name' => 'Roles',       'value' => $roles],
+        ];
+
+        return view('admin::dashboard.environment', compact('envs'));
     }
 }
