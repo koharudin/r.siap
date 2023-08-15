061405f8ded64b035cbc17a1eeec878a279cf87f
koharudin <koharudin.mail07@gmail.com>
2023-08-15 12:53:35 +0700

    download skpns

--- app/Admin/Controllers/ProfilePegawai/SKPNS_Controller.php
+++ app/Admin/Controllers/ProfilePegawai/SKPNS_Controller.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers\ProfilePegawai;
 
 use App\Admin\Selectable\GridPejabatPenetap;
+use App\Models\DokumenPegawai;
 use App\Models\Employee;
 use App\Models\JenisKP;
 use App\Models\JenisPensiun;
@@ -33,8 +34,20 @@ class SKPNS_Controller extends  ProfileController
     }
     public function form()
     {
+        
+        $dokumen  = DokumenPegawai::where('klasifikasi_id',$this->klasifikasi_id)->whereHas('obj_employee',function($query){
+            $query->where('id',$this->getProfileId());
+        })->get()->first();
 
         $form = new Form(new RiwayatSKPNS());
+        if($dokumen){
+            $url = route('admin.download.dokumen', [
+                'f' => base64_encode($dokumen->file)
+            ]);
+            $form->tools(function($tools) use($url){
+                $tools->add('<a href="'.$url.'" target="_blank" class="btn btn-sm btn-danger"><i class="fa fa-download"></i>&nbsp;&nbsp;Download SK</a>');
+            });
+        }
         $form->hidden('employee_id', 'ID');
         // Add an input box of type text
         $form->text('no_sk', 'NO. SURAT KEPUTUSAN')->required(true);
