ba4470cd7cef030d35ca3524ee4b335a84a91104
koharudin <koharudin.mail07@gmail.com>
2023-08-18 16:38:04 +0700

    build authorize url

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,16 @@
 */
 
 use App\Admin\Controllers\DaftarPegawaiController;
+use App\Models\Administrator;
 use App\Models\Agama;
 use Doctrine\DBAL\Schema\Index;
+use Encore\Admin\Facades\Admin;
 use Illuminate\Contracts\Cache\Store;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 
 Route::get('/', function () {
     return redirect('/admin');
@@ -25,16 +30,73 @@ Route::get('/', function () {
 Route::get('/test', function () {
     $minio = Storage::disk('minio');
     //$contents = $minio->imagePreviewUrl('images/upin-ipin.jpg');
-    
+
     $url = $minio->temporaryUrl(
-        'images/upin-ipin.jpg', now()->addMinutes(5)
+        'images/upin-ipin.jpg',
+        now()->addMinutes(5)
     );
 
     return $url;
 });
 
+function getUserId(){
+    $o_token = json_decode(session('sso_token'),true);
+    $response = Http::withHeaders([
+        'Authorization'=>'Bearer '. $o_token['access_token']
+    ])->get(env('SSO_USER_URL'), [
+    ]);
+    if($response->ok()){
+        return $response->json('user_id');
+    }
+}
+Route::middleware(['web'])->get('/test-login2',function(){
+    $user = Administrator::where('id',1)->get()->first();
+        //Auth::login($user);
+        //Auth::guard('admin')->login($user);
+    dd(session()->all());
+});
 Route::group([
-   // 'middleware'    => config('admin.route.middleware')
+    'middleware'    => []
 ], function (Router $router) {
-   
+    $router->get('/admin/test-login',function(Request $request){
+        $user = Administrator::where('id',2)->get()->first();
+        auth()->guard('admin')->login($user, true);
+       // dd(auth()->guard('admin'));
+        //Auth::guard('admin')->login($user);
+        //Auth::guard('admin')->login($user);
+        //Auth::guard('admin')->setUser($user);
+       // dd(Auth::user());
+        //dd($request->session());
+        //dd(session()->all());
+    });
+    $router->get('/test-token', function () {
+        $user = Administrator::where('id',getUserId())->get()->first();
+        Auth::login($user);
+        return redirect(route('admin.home'));
+    });
+    $router->get('/auth-mediakolaborasi-sso', function () {
+        $code = request('code');
+        if ($code) {
+            $response = Http::withHeaders([])->post(env('SSO_TOKEN_URL'), [
+                'grant_type' => 'authorization_code',
+                'client_id' => env('SSO_CLIENT_ID'),
+                'client_secret' => env('SSO_CLIENT_SECRET'),
+                'code' => $code
+            ]);
+            if ($response->ok()) {
+                $access_token = $response->json('access_token');
+                request()->session()->put('sso_token', $response->body());
+                session(['sso_token' => $response->body()]);
+                $user = Administrator::where('id',getUserId())->get()->first();
+                auth()->guard('admin')->login($user, true);
+                return redirect(route('admin.home'));
+            }
+        }
+        $params = [];
+        $params['client_id'] = env('SSO_CLIENT_ID');
+        $params['response_type'] ='code';
+        $params['state'] =md5(uniqid('sso_state'));
+
+        return redirect(env('SSO_AUTHORIZE_URL')."?".http_build_query($params));
+    })->name('auth.sso');
 });
