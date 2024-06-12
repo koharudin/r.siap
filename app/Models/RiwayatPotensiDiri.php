e4a3cc66541973e5b1f324e47f97704df60cee6c
Koharudin <koharudin.mail07@gmail.com>
2024-06-12 08:25:08 +0700

    acceptedClass

--- app/Models/RiwayatPotensiDiri.php
+++ app/Models/RiwayatPotensiDiri.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class RiwayatPotensiDiri extends Model
 {
     public $table  = 'riwayat_potensi_diri';
+
+    protected $fillable = ['tahun', 'tanggung_jawab', 'motivasi', 'minat'];
 }
