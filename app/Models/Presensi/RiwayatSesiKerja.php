7e9866836501144c5767932db65c8c57230648a1
Koharudin <koharudin.mail07@gmail.com>
2024-06-17 12:20:28 +0700

    fix data tidak terekam

--- app/Models/Presensi/RiwayatSesiKerja.php
+++ app/Models/Presensi/RiwayatSesiKerja.php
@@ -10,4 +10,6 @@ class RiwayatSesiKerja extends Model
      public $connection = 'db_presensi';
      public $table  = 'tbl_sesikerja';
      public $primaryKey = 'sesikerja_id';
+
+     public $dates = ["mulai","selesai"];
 }
