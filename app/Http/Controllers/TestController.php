614740f8cc5af0cf063c99ee1671a225a2886e8b
Koharudin <koharudin.mail07@gmail.com>
2024-06-18 10:48:42 +0700

    multi upload files

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -13,12 +13,16 @@ class TestController extends Controller
     }
 
     public function testUpload(){
-        $ext = ".txt";
-        $randomFileName = md5(uniqid()).$ext;
+       
         $disk_layanan = Storage::disk("minio_layanan");
-        $file = request()->file("cek");
-        $disk_layanan->putFileAs("/",$file,"axx.jpg");
-        $disk_layanan->put($randomFileName,"andin anak solehah");
-        return response()->json(request());
+        $files = request()->file("files");
+        foreach($files as $file){
+            $ext = ".".$file->getClientOriginalExtension();
+            $randomFileName = md5(uniqid()).$ext;
+            $disk_layanan->putFileAs("/",$file,$randomFileName);
+        }
+        
+       // $disk_layanan->put($randomFileName,"andin anak solehah");
+        return response()->json("jumlah file " .(sizeof($files)));
     }
 }
