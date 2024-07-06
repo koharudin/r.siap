c98c6590d4bab094b1a60b40b6c75c834b1fdc13
Koharudin <koharudin.mail07@gmail.com>
2024-07-06 07:26:09 +0700

    cek on submit&verif

--- app/Models/VerifiedClass/SuratPerintah.php
+++ app/Models/VerifiedClass/SuratPerintah.php
@@ -12,6 +12,7 @@ use App\Models\Request;
 use App\Models\RiwayatPotensiDiri;
 use Carbon\Carbon;
 use Exception;
+use Illuminate\Support\Facades\DB;
 
 class SuratPerintah extends AcceptedClass
 {
@@ -27,12 +28,16 @@ class SuratPerintah extends AcceptedClass
             throw new Exception("Pegawai Presensi tidak ditemukan");
         }
         $new_data = json_decode(request()->input("new_data"));
+        $tgl_mulai = json_decode(request()->input("tgl_mulai"));
+        $tgl_selesai = json_decode(request()->input("tgl_selesai"));
         foreach ($new_data->employees as $employee) {
             $ep = EmployeePresensi::where("nipp", $employee->nip_baru)->get()->first();
-            //RiwayatIzin::where("no_pekerja")
-            //throw new Exception ("employee ".$employee->nip_baru);
+            $c = DB::connection("db_presensi")->select("CALL cek_ijin(?,?,?, ?,?) ", array($ep->nomor_pekerja, $tgl_mulai, $tgl_selesai, 1, 1));
+            $result =  $c[0]->cek;
+            if ($result == 0) {
+                throw new Exception("Pegawai sudah ada ijin dalam tanggal tersebut");
+            }
         }
-        //throw new Exception ("cek surat perintah");
     }
     public function hook(Request $request)
     {
@@ -53,8 +58,27 @@ class SuratPerintah extends AcceptedClass
             if (!$employees) {
                 throw new Exception("Tidak ada daftar pegawai");
             }
+            $tgl_mulai =  @$data['new_data']['tgl_mulai'];
+            $tgl_selesai =  @$data['new_data']['tgl_selesai'];
+            foreach($employees as $employee){
+                $e = Employee::whereRaw('nip_baru = ?', [$employee["nip_baru"]])->first();
+                if (!$e) {
+                    throw new Exception("Pegawai tidak ditemukan");
+                }
+                $ep = EmployeePresensi::where("nipp", $e->nip_baru)->get()->first();
+                if (!$ep) {
+                    throw new Exception("Pegawai Presensi tidak ditemukan");
+                }
+                $c = DB::connection("db_presensi")->select("CALL cek_ijin(?,?,?, ?,?) ", array($ep->nomor_pekerja, $tgl_mulai, $tgl_selesai, 1, 1));
+                $result =  $c[0]->cek;
+                if ($result == 0) {
+                    throw new Exception("Pegawai sudah ada ijin dalam tanggal tersebut");
+                }
+            }
+
             foreach ($employees as $employee) {
                 $r = new RiwayatIzin();
+                $ep = EmployeePresensi::where("nipp", @$employee['nip_baru'])->get()->first();
                 $r->no_pekerja = $ep->nomor_pekerja;
                 $r->no_sk = @$data['new_data']['no_sk'];
                 $r->tgl_sk = @$data['new_data']['tgl_sk'];
