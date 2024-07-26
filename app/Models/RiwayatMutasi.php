77b436e576e67bbc6a8fd4052fcbd051658576f1
Koharudin <koharudin.mail07@gmail.com>
2024-07-26 18:33:06 +0700

    sync mutasi

--- app/Models/RiwayatMutasi.php
+++ app/Models/RiwayatMutasi.php
@@ -22,4 +22,46 @@ class RiwayatMutasi extends Model
         'tgl_sk' => 'datetime:Y-m-d',
         'tmt_sk' => 'datetime:Y-m-d'
     ];
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            // ... code here
+        });
+
+        self::created(function ($model) {
+
+            $last_mutasi = $model->obj_pegawai->obj_riwayat_mutasi->last();
+            $employee =   $model->obj_pegawai;
+            $employee->unit_id = $last_mutasi->satker_id_baru;
+            $employee->save();
+        });
+
+        self::updating(function ($model) {
+            // ... code here
+        });
+
+        self::updated(function ($model) {
+            $last_mutasi = $model->obj_pegawai->obj_riwayat_mutasi->last();
+            $employee = $model->obj_pegawai;
+            $employee->unit_id = $last_mutasi->satker_id_baru;
+            $employee->save();
+        });
+
+        self::deleting(function ($model) {
+            // ... code here
+        });
+
+        self::deleted(function ($model) {
+            $last_mutasi = $model->obj_pegawai->obj_riwayat_mutasi->last();
+            $employee = $model->obj_pegawai;
+            $employee->unit_id = $last_mutasi->satker_id_baru;
+            $employee->save();
+        });
+    }
+    public function obj_pegawai()
+    {
+        return $this->hasOne(Employee::class, 'id', 'employee_id');
+    }
 }
